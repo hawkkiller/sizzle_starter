9398bf1ac8e41ce9da11e1d3075becec5c53b1ed
Mykhailo Lazebnyi <62852417+hawkkiller@users.noreply.github.com>
2022-09-16 19:26:39 +0200

    [#2]: `Setup gitignore and clean project` (#4)

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
+import 'package:blaze_starter/src/app.dart';
 import 'package:flutter/material.dart';
 
-import 'src/app.dart';
-import 'src/settings/settings_controller.dart';
-import 'src/settings/settings_service.dart';
-
 void main() async {
-  final settingsController = SettingsController(SettingsService());
-
-  await settingsController.loadSettings();
-
-  runApp(MyApp(settingsController: settingsController));
+  runApp(const App());
 }
