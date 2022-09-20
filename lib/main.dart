2129ff615c790e9bda0969148d10d716d08aca45
Platon <83786662+Alphorov@users.noreply.github.com>
2022-09-21 00:20:28 +0200

    code format

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:blaze_starter/src/core/widget/app.dart';
 import 'package:flutter/material.dart';
 
-void main() async {
-  runApp(const App());
-}
+void main() async => runApp(const App());
