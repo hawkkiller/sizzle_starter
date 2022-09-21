a37d840c81eef26a3d7bc0033fd010464f65cb27
Mykhailo Lazebnyi <62852417+hawkkiller@users.noreply.github.com>
2022-09-21 21:07:58 +0200

    [#20]: `Initialization logic implemented` (#31)

---
+++ lib/src/feature/initialization/model/initialization_progress.dart
@@ -0,0 +1,60 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'initialization_progress.freezed.dart';
+
+@freezed
+class ResultingRepositories with _$ResultingRepositories {
+  const factory ResultingRepositories() = _ResultingRepositories;
+}
+
+@freezed
+class ResultingDependencies with _$ResultingDependencies {
+  const factory ResultingDependencies({
+    required SharedPreferences preferences,
+  }) = _ResultingDependencies;
+
+  const ResultingDependencies._();
+}
+
+@freezed
+class InitializationRepositories with _$InitializationRepositories {
+  const factory InitializationRepositories() = _InitializationRepositories;
+
+  ResultingRepositories result() => const ResultingRepositories();
+
+  const InitializationRepositories._();
+}
+
+@freezed
+class InitializationDependencies with _$InitializationDependencies {
+  const factory InitializationDependencies({
+    SharedPreferences? preferences,
+  }) = _InitializationDependencies;
+
+  ResultingDependencies result() => ResultingDependencies(
+        preferences: preferences!,
+      );
+
+  const InitializationDependencies._();
+}
+
+@freezed
+class InitializationResult with _$InitializationResult {
+  const factory InitializationResult({
+    required ResultingDependencies dependencies,
+    required ResultingRepositories repositories,
+    required int stepCount,
+    required int msSpent,
+  }) = _InitializationResult;
+}
+
+@freezed
+class InitializationWrapper with _$InitializationWrapper {
+  const factory InitializationWrapper({
+    required InitializationRepositories repositories,
+    required InitializationDependencies dependencies,
+  }) = _InitializationWrapper;
+
+  const InitializationWrapper._();
+}
