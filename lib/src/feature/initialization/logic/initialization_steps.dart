d346b5cfa2de697191112a24d21167b7309423c4
Mihail <miskadl09@gmail.com>
2022-09-25 00:01:02 +0200

    [#37]: `Setup Firebase Analytics`

--- lib/src/feature/initialization/logic/initialization_steps.dart
+++ lib/src/feature/initialization/logic/initialization_steps.dart
@@ -2,12 +2,14 @@ import 'dart:async';
 
 import 'package:blaze_starter/src/core/router/routes.dart';
 import 'package:blaze_starter/src/feature/initialization/model/initialization_progress.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-typedef StepAction = FutureOr<InitializationWrapper> Function(
+typedef StepAction = FutureOr<InitializationWrapper>? Function(
   InitializationWrapper wrapper,
   InitializationDependencies,
   InitializationRepositories,
@@ -16,6 +18,11 @@ typedef StepDescription = String;
 
 mixin InitializationSteps {
   final steps = <StepDescription, StepAction>{
+    'Init Firebase': (wrapper, p1, p2) {
+      Firebase.initializeApp(
+        // setup options here options: 
+      );
+    },
     'Init Shared Preferences': (wrapper, dep, rep) async {
       final sharedPreferences = await SharedPreferences.getInstance();
       return wrapper.copyWith(
@@ -26,6 +33,9 @@ mixin InitializationSteps {
       final router = GoRouter(
         observers: <NavigatorObserver>[
           SentryNavigatorObserver(),
+          FirebaseAnalyticsObserver(
+            analytics: FirebaseAnalytics.instance,
+          ),
         ],
         routes: $appRoutes,
       );
