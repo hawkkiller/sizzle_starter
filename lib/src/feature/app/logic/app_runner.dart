bbbd9a002bb826d3ec5a943e7daa42acee4cb988
Michael Lazebnyi <62852417+hawkkiller@users.noreply.github.com>
2022-11-07 13:42:24 +0200

    [Feature]: `Moved app widgets to app feature` (#61)

--- lib/src/feature/app/logic/app_runner.dart
+++ lib/src/feature/app/logic/app_runner.dart
@@ -1,5 +1,5 @@
 import 'package:blaze_starter/src/core/model/initialization_hook.dart';
-import 'package:blaze_starter/src/core/widget/app.dart';
+import 'package:blaze_starter/src/feature/app/widget/app.dart';
 import 'package:blaze_starter/src/feature/initialization/logic/initialization_processor.dart';
 import 'package:blaze_starter/src/feature/initialization/logic/initialization_steps.dart';
 import 'package:flutter/material.dart';
