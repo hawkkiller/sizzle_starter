3157ab4a43a918137edc2b8e55b08b371503b916
Mihail <miskadl09@gmail.com>
2023-01-31 17:07:53 +0100

    Initial rest_client implementation

---
+++ lib/src/core/exception/network_exception.dart
@@ -0,0 +1,36 @@
+import 'package:meta/meta.dart';
+import 'package:sizzle_starter/src/core/utils/annotation.dart';
+
+@immutable
+@exception
+abstract class NetworkException implements Exception {}
+
+@immutable
+@exception
+class RestClientException implements NetworkException {
+  const RestClientException({
+    this.message,
+    this.statusCode,
+  });
+
+  final String? message;
+  final int? statusCode;
+
+  @override
+  String toString() => 'RestClientException(message: $message, statusCode: $statusCode)';
+}
+
+@immutable
+@exception
+class InternalServerException implements NetworkException {
+  const InternalServerException({
+    this.message,
+    this.statusCode,
+  });
+
+  final String? message;
+  final int? statusCode;
+
+  @override
+  String toString() => 'InternalServerErrorException(message: $message, statusCode: $statusCode)';
+}
