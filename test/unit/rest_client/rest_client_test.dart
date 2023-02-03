dd07eac65dedd7aacf0b16ca3e82b8d83a55f725
Mihail <miskadl09@gmail.com>
2023-02-03 12:09:56 +0100

    Written tests for test_decode_response

--- test/unit/rest_client/rest_client_test.dart
+++ test/unit/rest_client/rest_client_test.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
 import 'package:sizzle_starter/src/core/data/rest_client.dart';
+import 'package:sizzle_starter/src/core/exception/network_exception.dart';
 
 void main() {
   group(
@@ -47,87 +49,161 @@ void main() {
           },
         );
       });
-      group(
-        'Build request',
-        () {
-          late final Uri baseUri;
-          setUpAll(
-            () => baseUri = Uri.parse('https://example.com'),
-          );
-          test(
-            'should build a valid GET request',
-            () {
-              final request = RestClient.buildRequest(
-                baseUri: baseUri,
-                path: 'path',
-                method: 'GET',
-                headers: {
-                  'key': 'value',
-                },
-              );
-              expect(request.method, 'GET');
-              expect(request.url.toString(), 'https://example.com/path');
-              expect(request.headers['key'], 'value');
+      group('Build request', () {
+        late final Uri baseUri;
+        setUpAll(
+          () => baseUri = Uri.parse('https://example.com'),
+        );
+        test(
+          'should build a valid GET request',
+          () {
+            final request = RestClient.buildRequest(
+              baseUri: baseUri,
+              path: 'path',
+              method: 'GET',
+              headers: {
+                'key': 'value',
+              },
+            );
+            expect(request.method, 'GET');
+            expect(request.url.toString(), 'https://example.com/path');
+            expect(request.headers['key'], 'value');
+          },
+        );
+        test(
+          'should build a valid POST request',
+          () {
+            final request = RestClient.buildRequest(
+              baseUri: baseUri,
+              path: 'path',
+              method: 'POST',
+              headers: {
+                'key': 'value',
+              },
+              body: {
+                'bodykey': 'bodyvalue',
+              },
+            );
+            expect(request.method, 'POST');
+            expect(request.url.toString(), 'https://example.com/path');
+            expect(request.headers['key'], 'value');
+            expect(request.body, '{"bodykey":"bodyvalue"}');
+          },
+        );
+        test(
+          'should build a valid PUT request',
+          () {
+            final request = RestClient.buildRequest(
+              baseUri: baseUri,
+              path: 'path',
+              method: 'PUT',
+              headers: {
+                'key': 'value',
+              },
+              body: {
+                'bodykey': 'bodyvalue',
+              },
+            );
+            expect(request.method, 'PUT');
+            expect(request.url.toString(), 'https://example.com/path');
+            expect(request.headers['key'], 'value');
+            expect(request.body, '{"bodykey":"bodyvalue"}');
+          },
+        );
+        test(
+          'should build a valid DELETE request',
+          () {
+            final request = RestClient.buildRequest(
+              baseUri: baseUri,
+              path: 'path',
+              method: 'DELETE',
+              headers: {
+                'key': 'value',
+              },
+            );
+            expect(request.method, 'DELETE');
+            expect(request.url.toString(), 'https://example.com/path');
+            expect(request.headers['key'], 'value');
+          },
+        );
+      });
+      group('Decode response', () {
+        test(
+          'Throw exception if response json is not in right format',
+          () {
+            final response = http.Response(
+              '{"key":"value"}',
+              200,
+              headers: {
+                'content-type': 'application/json',
+              },
+            );
+            expect(
+              () => RestClient.decodeResponse(response),
+              throwsA(isA<InternalServerException>()),
+              reason: 'Should throw an exception for an invalid JSON response',
+            );
+          },
+        );
+        test(
+          'should throw an exception for an invalid JSON response',
+          () {
+            final response = http.Response(
+              'invalid',
+              200,
+              headers: {
+                'content-type': 'application/json',
+              },
+            );
+            expect(
+              () => RestClient.decodeResponse(response),
+              throwsA(isA<InternalServerException>()),
+            );
+          },
+        );
+        test(
+          'should throw an exception for an invalid content type',
+          () {
+            final response = http.Response(
+              'invalid',
+              200,
+              headers: {
+                'content-type': 'text/html',
+              },
+            );
+            expect(
+              () => RestClient.decodeResponse(response),
+              throwsA(isA<InternalServerException>()),
+            );
+          },
+        );
+        test('Should decode error message correctly', () {
+          final response = http.Response(
+            '{"message": "error message"}',
+            200,
+            headers: {
+              'content-type': 'application/json',
             },
           );
-          test(
-            'should build a valid POST request',
-            () {
-              final request = RestClient.buildRequest(
-                baseUri: baseUri,
-                path: 'path',
-                method: 'POST',
-                headers: {
-                  'key': 'value',
-                },
-                body: {
-                  'bodykey': 'bodyvalue',
-                },
-              );
-              expect(request.method, 'POST');
-              expect(request.url.toString(), 'https://example.com/path');
-              expect(request.headers['key'], 'value');
-              expect(request.body, '{"bodykey":"bodyvalue"}');
+          expect(
+            () => RestClient.decodeResponse(response),
+            throwsA(isA<RestClientException>()),
+          );
+        });
+        test('Should decode success data correctly', () {
+          final response = http.Response(
+            '{"data": {"key": "value"}}',
+            200,
+            headers: {
+              'content-type': 'application/json',
             },
           );
-          test(
-            'should build a valid PUT request',
-            () {
-              final request = RestClient.buildRequest(
-                baseUri: baseUri,
-                path: 'path',
-                method: 'PUT',
-                headers: {
-                  'key': 'value',
-                },
-                body: {
-                  'bodykey': 'bodyvalue',
-                },
-              );
-              expect(request.method, 'PUT');
-              expect(request.url.toString(), 'https://example.com/path');
-              expect(request.headers['key'], 'value');
-              expect(request.body, '{"bodykey":"bodyvalue"}');
-            },
+          expect(
+            () => RestClient.decodeResponse(response),
+            returnsNormally,
           );
-          test(
-            'should build a valid DELETE request',
-            () {
-              final request = RestClient.buildRequest(
-                baseUri: baseUri,
-                path: 'path',
-                method: 'DELETE',
-                headers: {
-                  'key': 'value',
-                },
-              );
-              expect(request.method, 'DELETE');
-              expect(request.url.toString(), 'https://example.com/path');
-              expect(request.headers['key'], 'value');
-            },
-          );
-        },
-      );
+        });
+      });
     },
   );
 }
