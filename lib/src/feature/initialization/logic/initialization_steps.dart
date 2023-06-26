8113eb0a081f79bca26d299ab42726efa3af482f
Michael Lazebny <michael@lazebny.io>
2023-06-26 17:06:03 +0200

    Implemented Router Scope

--- lib/src/feature/initialization/logic/initialization_steps.dart
+++ lib/src/feature/initialization/logic/initialization_steps.dart
@@ -1,21 +1,19 @@
 import 'dart:async';
 
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:sizzle_starter/src/core/router/router.dart';
 import 'package:sizzle_starter/src/feature/initialization/model/dependencies.dart';
 
 typedef StepAction = FutureOr<void>? Function(Dependencies$Mutable progress);
 
-///
+/// The initialization steps, which are executed in the order they are defined.
+/// 
+/// The [Dependencies] object is passed to each step, which allows the step to
+/// set the dependency, and the next step to use it.
 mixin InitializationSteps {
   final initializationSteps = <String, StepAction>{
     'Shared Preferences': (progress) async {
       final sharedPreferences = await SharedPreferences.getInstance();
       progress.sharedPreferences = sharedPreferences;
     },
-    'App Router': (progress) {
-      final router = AppRouter();
-      progress.router = router;
-    }
   };
 }
