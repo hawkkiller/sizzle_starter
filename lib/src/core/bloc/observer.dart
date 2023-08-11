aa179f4c36c08053e211b54aadd2ada731cbc1c9
Michael Lazebny <michael@lazebny.io>
2023-08-11 21:02:11 +0200

    lints

--- lib/src/core/bloc/observer.dart
+++ lib/src/core/bloc/observer.dart
@@ -2,6 +2,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sizzle_starter/src/core/utils/extensions/string_extension.dart';
 import 'package:sizzle_starter/src/core/utils/logger.dart';
 
+/// [BlocObserver] which logs all bloc state changes, errors and events.
 class AppBlocObserver extends BlocObserver {
   @override
   void onTransition(
