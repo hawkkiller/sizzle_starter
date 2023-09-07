5fa7d988bd78f3952a09532f4c4881770b25dcb5
Michael Lazebny <62852417+hawkkiller@users.noreply.github.com>
2023-09-07 18:10:57 +0200

    Refactored theme & structures (#248)
    - Removed [AppThemeMode], replaced with [ThemeMode]
    - Renamed setOrRemove to setIfNullRemove
    - Removed theme.dart file
    - Updated home_screen.dart

--- lib/src/feature/app/model/app_theme.dart
+++ lib/src/feature/app/model/app_theme.dart
@@ -1,41 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-/// {@template app_theme_type}
-/// The type of theme to use.
-/// {@endtemplate}
-enum AppThemeMode {
-  /// Light theme.
-  light,
-
-  /// Dark theme.
-  dark,
-
-  /// System theme.
-  system;
-
-  /// Whether this is a system theme.
-  bool get isSystem => switch (this) {
-        AppThemeMode.system => true,
-        _ => false,
-      };
-
-  @override
-  String toString() => switch (this) {
-        AppThemeMode.light => 'light',
-        AppThemeMode.dark => 'dark',
-        AppThemeMode.system => 'system',
-      };
-
-  /// Creates a [AppThemeMode] from a [String].
-  static AppThemeMode fromString(String value) => switch (value) {
-        'light' => AppThemeMode.light,
-        'dark' => AppThemeMode.dark,
-        'system' => AppThemeMode.system,
-        _ => throw Exception('Unknown AppThemeMode: $value'),
-      };
-}
-
 /// {@template app_theme}
 /// An immutable class that holds properties needed
 /// to build a [ThemeData] for the app.
@@ -43,92 +8,66 @@ enum AppThemeMode {
 @immutable
 final class AppTheme with Diagnosticable {
   /// The type of theme to use.
-  final AppThemeMode type;
+  final ThemeMode mode;
 
   /// The seed color to generate the [ColorScheme] from.
   final Color? seed;
 
   /// {@macro app_theme}
-  const AppTheme({
-    required this.type,
+  AppTheme({
+    required this.mode,
     this.seed,
-  });
+  })  : darkTheme = ThemeData(
+          colorSchemeSeed: seed ?? Colors.pink,
+          brightness: Brightness.dark,
+          useMaterial3: true,
+        ),
+        lightTheme = ThemeData(
+          colorSchemeSeed: seed ?? Colors.pink,
+          brightness: Brightness.light,
+          useMaterial3: true,
+        );
 
-  /// Dark theme
-  static const dark = AppTheme(type: AppThemeMode.dark);
+  /// Light mode [AppTheme].
+  static final light = AppTheme(mode: ThemeMode.light);
 
-  /// Light theme
-  static const light = AppTheme(type: AppThemeMode.light);
+  /// Dark mode [AppTheme].
+  static final dark = AppTheme(mode: ThemeMode.dark);
 
-  /// System theme
-  static const system = AppTheme(type: AppThemeMode.system);
+  /// System mode [AppTheme].
+  static final system = AppTheme(mode: ThemeMode.system);
 
   /// All the light [AppTheme]s.
-  static final lightValues = [
+  static final values = [
     ...List.generate(
       Colors.primaries.length,
       (index) => AppTheme(
         seed: Colors.primaries[index],
-        type: AppThemeMode.light,
+        mode: ThemeMode.system,
       ),
     ),
   ];
 
-  /// All the dark [AppTheme]s.
-  static final darkValues = [
-    ...List.generate(
-      Colors.primaries.length,
-      (index) => AppTheme(
-        seed: Colors.primaries[index],
-        type: AppThemeMode.dark,
-      ),
-    ),
-  ];
+  /// The dark [ThemeData] for this [AppTheme].
+  final ThemeData darkTheme;
 
-  /// Get the dark [ThemeData] for this [AppTheme].
-  ThemeData get darkTheme {
-    if (seed != null) {
-      return ThemeData(
-        colorSchemeSeed: seed,
-        brightness: Brightness.dark,
-        useMaterial3: true,
-      );
-    }
+  /// The light [ThemeData] for this [AppTheme].
+  final ThemeData lightTheme;
 
-    // Define there your default dark theme.
-    return ThemeData(
-      brightness: Brightness.dark,
-      useMaterial3: true,
-      colorSchemeSeed: Colors.pink,
-    );
-  }
-
-  /// Get the light [ThemeData] for this [AppTheme].
-  ThemeData get lightTheme {
-    if (seed != null) {
-      return ThemeData(
-        colorSchemeSeed: seed,
-        brightness: Brightness.light,
-        useMaterial3: true,
-      );
-    }
-
-    // Define there your default light theme.
-    return ThemeData(
-      brightness: Brightness.light,
-      useMaterial3: true,
-      colorSchemeSeed: Colors.pink,
-    );
-  }
-
-  /// Get the [ThemeMode] for this [AppTheme].
-  ThemeMode get themeMode {
-    if (type == AppThemeMode.system) {
-      return ThemeMode.system;
-    } else if (type == AppThemeMode.light) {
-      return ThemeMode.light;
-    } else {
-      return ThemeMode.dark;
+  /// The [ThemeData] for this [AppTheme].
+  /// This is computed based on the [mode].
+  ///
+  /// Could be useful for theme showcase.
+  ThemeData computeTheme(BuildContext context) {
+    switch (mode) {
+      case ThemeMode.light:
+        return lightTheme;
+      case ThemeMode.dark:
+        return darkTheme;
+      case ThemeMode.system:
+        return MediaQuery.platformBrightnessOf(context) == Brightness.dark
+            ? darkTheme
+            : lightTheme;
     }
   }
 
@@ -136,7 +75,7 @@ final class AppTheme with Diagnosticable {
   void debugFillProperties(DiagnosticPropertiesBuilder properties) {
     super.debugFillProperties(properties);
     properties.add(ColorProperty('seed', seed));
-    properties.add(EnumProperty<AppThemeMode>('type', type));
+    properties.add(EnumProperty<ThemeMode>('type', mode));
     properties.add(DiagnosticsProperty<ThemeData>('lightTheme', lightTheme));
     properties.add(DiagnosticsProperty<ThemeData>('darkTheme', darkTheme));
   }
@@ -147,8 +86,8 @@ final class AppTheme with Diagnosticable {
       other is AppTheme &&
           runtimeType == other.runtimeType &&
           seed == other.seed &&
-          type == other.type;
+          mode == other.mode;
 
   @override
-  int get hashCode => type.hashCode ^ seed.hashCode;
+  int get hashCode => mode.hashCode ^ seed.hashCode;
 }
