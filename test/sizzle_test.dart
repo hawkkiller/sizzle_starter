dd602c9e785d0ac61a36b50b3d7ebc50e877bafd
Michael Lazebny <michael@lazebny.io>
2023-09-17 18:56:09 +0200

    Updated CI and fixed tests

--- test/sizzle_test.dart
+++ test/sizzle_test.dart
@@ -1,11 +1,5 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'rest_client_test.dart' as rest_client_test;
 
 void main() {
-  test(
-    'Sizzle Starter tests',
-    () {
-      rest_client_test.main();
-    },
-  );
+  rest_client_test.main();
 }
