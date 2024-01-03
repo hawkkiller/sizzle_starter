94b54e5e9cb2a9e1865a976e95f8d8c80b12b6ea
Michael Lazebny <62852417+hawkkiller@users.noreply.github.com>
2024-01-03 15:53:07 +0100

    refactor: Changed depedencies initialization (#308)
    * refactor: Changed depedencies initialization
    
    - Simplified initialization processor
    - Removed initialization steps and hook
    - Now dependencies are initialized using a simple method call
    - Dependencies model doesn't use late initialization anymore
    
    * feat: Added failure app for initialization

--- lib/src/feature/app/logic/app_runner.dart
+++ lib/src/feature/app/logic/app_runner.dart
@@ -3,26 +3,25 @@ import 'dart:async';
 import 'package:bloc_concurrency/bloc_concurrency.dart' as bloc_concurrency;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:sizzle_starter/src/core/utils/app_bloc_observer.dart';
 import 'package:sizzle_starter/src/core/utils/logger.dart';
 import 'package:sizzle_starter/src/feature/app/widget/app.dart';
 import 'package:sizzle_starter/src/feature/initialization/logic/initialization_processor.dart';
-import 'package:sizzle_starter/src/feature/initialization/logic/initialization_steps.dart';
-import 'package:sizzle_starter/src/feature/initialization/model/initialization_hook.dart';
+import 'package:sizzle_starter/src/feature/initialization/widget/initialization_failed_app.dart';
 
+/// {@template app_runner}
 /// A class which is responsible for initialization and running the app.
-final class AppRunner
-    with
-        InitializationSteps,
-        InitializationProcessor,
-        InitializationFactoryImpl {
+/// {@endtemplate}
+final class AppRunner with InitializationFactoryImpl {
+  /// {@macro app_runner}
+  const AppRunner();
+
   /// Start the initialization and in case of success run application
-  Future<void> initializeAndRun(InitializationHook hook) async {
+  Future<void> initializeAndRun() async {
     final binding = WidgetsFlutterBinding.ensureInitialized();
 
     // Preserve splash screen
-    FlutterNativeSplash.preserve(widgetsBinding: binding);
+    binding.deferFirstFrame();
 
     // Override logging
     FlutterError.onError = logger.logFlutterError;
@@ -33,16 +32,34 @@ final class AppRunner
     Bloc.observer = const AppBlocObserver();
     Bloc.transformer = bloc_concurrency.sequential();
 
-    final result = await processInitialization(
-      steps: initializationSteps,
-      hook: hook,
-      factory: this,
+    final environmentStore = getEnvironmentStore();
+
+    final initializationProcessor = InitializationProcessor(
+      trackingManager: createTrackingManager(environmentStore),
+      environmentStore: environmentStore,
     );
 
-    // Allow rendering
-    FlutterNativeSplash.remove();
+    Future<void> initializeAndRun() async {
+      try {
+        final result = await initializationProcessor.initialize();
+        // Attach this widget to the root of the tree.
+        runApp(App(result: result));
+      } catch (e, stackTrace) {
+        logger.error('Initialization failed', error: e, stackTrace: stackTrace);
+        runApp(
+          InitializationFailedApp(
+            error: e,
+            stackTrace: stackTrace,
+            retryInitialization: initializeAndRun,
+          ),
+        );
+      } finally {
+        // Allow rendering
+        binding.allowFirstFrame();
+      }
+    }
 
-    // Attach this widget to the root of the tree.
-    runApp(App(result: result));
+    // Run the app
+    await initializeAndRun();
   }
 }
--- lib/src/feature/initialization/model/dependencies.dart
+++ lib/src/feature/initialization/model/dependencies.dart
@@ -1,22 +1,21 @@
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sizzle_starter/src/feature/settings/bloc/settings_bloc.dart';
-import 'package:sizzle_starter/src/feature/settings/data/settings_repository.dart';
 
 /// {@template dependencies}
 /// Dependencies container
 /// {@endtemplate}
 base class Dependencies {
   /// {@macro dependencies}
-  Dependencies();
+  const Dependencies({
+    required this.sharedPreferences,
+    required this.settingsBloc,
+  });
 
   /// [SharedPreferences] instance, used to store Key-Value pairs.
-  late final SharedPreferences sharedPreferences;
-
-  /// [SettingsRepository] instance, used to manage theme and locale.
-  late final SettingsRepository settingsRepository;
+  final SharedPreferences sharedPreferences;
 
   /// [SettingsBloc] instance, used to manage theme and locale.
-  late final SettingsBloc settingsBloc;
+  final SettingsBloc settingsBloc;
 }
 
 /// {@template initialization_result}
@@ -26,15 +25,11 @@ final class InitializationResult {
   /// {@macro initialization_result}
   const InitializationResult({
     required this.dependencies,
-    required this.stepCount,
     required this.msSpent,
   });
 
   /// The dependencies
   final Dependencies dependencies;
-
-  /// The number of steps
-  final int stepCount;
 
   /// The number of milliseconds spent
   final int msSpent;
@@ -42,7 +37,6 @@ final class InitializationResult {
   @override
   String toString() => '$InitializationResult('
       'dependencies: $dependencies, '
-      'stepCount: $stepCount, '
       'msSpent: $msSpent'
       ')';
 }
