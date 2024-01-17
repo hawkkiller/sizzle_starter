40a847b69c921a2c47b6c5f2c16c79896da5da98
Michael Lazebny <michael@lazebny.io>
2024-01-17 20:33:13 +0100

    fix: add query executor to constructor

--- lib/src/core/components/database/src/tables/todos_table.dart
+++ lib/src/core/components/database/src/tables/todos_table.dart
@@ -2,7 +2,7 @@
 import 'package:drift/drift.dart';
 
 /// Todos table definition
-final class TodosTable extends Table {
+class TodosTable extends Table {
   /// The identifier for this todo.
   IntColumn get id => integer().autoIncrement()();
 
