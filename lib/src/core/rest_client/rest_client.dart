5f875f2e8034301da27dd1d2589715edc549d384
Michael Lazebny <michael@lazebny.io>
2024-04-28 08:55:17 +0200

    test: update structure

--- lib/src/core/rest_client/rest_client.dart
+++ lib/src/core/rest_client/rest_client.dart
@@ -6,4 +6,3 @@ export 'src/exception/rest_client_exception.dart';
 export 'src/http/rest_client_http.dart';
 export 'src/rest_client.dart';
 export 'src/rest_client_base.dart';
-export 'src/rest_client_dio.dart';
