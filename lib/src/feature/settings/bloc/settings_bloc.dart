a669183474d0172623b4c7ffddf17deba12d88ef
Michael Lazebny <michael@lazebny.io>
2024-05-13 21:44:43 +0200

    Migrated settings state from freezed

--- lib/src/feature/settings/bloc/settings_bloc.dart
+++ lib/src/feature/settings/bloc/settings_bloc.dart
@@ -1,65 +1,9 @@
-import 'package:flutter/material.dart' show Locale;
+import 'dart:ui' show Locale;
+
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:sizzle_starter/src/feature/app/model/app_theme.dart';
 import 'package:sizzle_starter/src/feature/settings/data/locale_repository.dart';
 import 'package:sizzle_starter/src/feature/settings/data/theme_repository.dart';
-
-part 'settings_bloc.freezed.dart';
-
-/// States for the [SettingsBloc].
-@freezed
-sealed class SettingsState with _$SettingsState {
-  const SettingsState._();
-
-  /// Idle state for the [SettingsBloc].
-  const factory SettingsState.idle({
-    /// The current locale.
-    Locale? locale,
-
-    /// The current theme mode.
-    AppTheme? appTheme,
-  }) = _IdleSettingsState;
-
-  /// Processing state for the [SettingsBloc].
-  const factory SettingsState.processing({
-    /// The current locale.
-    Locale? locale,
-
-    /// The current theme mode.
-    AppTheme? appTheme,
-  }) = _ProcessingSettingsState;
-
-  /// Error state for the [SettingsBloc].
-  const factory SettingsState.error({
-    /// The error message.
-    required Object cause,
-
-    /// The current locale.
-    Locale? locale,
-
-    /// The current theme mode.
-    AppTheme? appTheme,
-  }) = _ErrorSettingsState;
-}
-
-/// Events for the [SettingsBloc].
-@freezed
-sealed class SettingsEvent with _$SettingsEvent {
-  const SettingsEvent._();
-
-  /// Event to update the theme mode.
-  const factory SettingsEvent.updateTheme({
-    /// The new theme mode.
-    required AppTheme appTheme,
-  }) = _UpdateThemeSettingsEvent;
-
-  /// Event to update the locale.
-  const factory SettingsEvent.updateLocale({
-    /// The new locale.
-    required Locale locale,
-  }) = _UpdateLocaleSettingsEvent;
-}
 
 /// {@template settings_bloc}
 /// A [Bloc] that handles the settings.
@@ -74,10 +18,10 @@ final class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
         _themeRepo = themeRepository,
         super(initialState) {
     on<SettingsEvent>(
-      (event, emit) => event.map(
-        updateTheme: (event) => _updateTheme(event, emit),
-        updateLocale: (event) => _updateLocale(event, emit),
-      ),
+      (event, emit) => switch (event) {
+        final _UpdateLocaleSettingsEvent e => _updateLocale(e, emit),
+        final _UpdateThemeSettingsEvent e => _updateTheme(e, emit),
+      },
     );
   }
 
@@ -141,4 +85,151 @@ final class SettingsBloc extends Bloc<SettingsEvent, SettingsState> {
       rethrow;
     }
   }
+}
+
+/// States for the [SettingsBloc].
+sealed class SettingsState {
+  const SettingsState({this.locale, this.appTheme});
+
+  /// Application locale.
+  final Locale? locale;
+
+  /// Data class used to represent the state of theme.
+  final AppTheme? appTheme;
+
+  /// Idle state for the [SettingsBloc].
+  const factory SettingsState.idle({Locale? locale, AppTheme? appTheme}) =
+      _IdleSettingsState;
+
+  /// Processing state for the [SettingsBloc].
+  const factory SettingsState.processing({Locale? locale, AppTheme? appTheme}) =
+      _ProcessingSettingsState;
+
+  /// Error state for the [SettingsBloc].
+  const factory SettingsState.error({
+    required Object cause,
+    Locale? locale,
+    AppTheme? appTheme,
+  }) = _ErrorSettingsState;
+}
+
+final class _IdleSettingsState extends SettingsState {
+  const _IdleSettingsState({super.locale, super.appTheme});
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is _IdleSettingsState &&
+        other.locale == locale &&
+        other.appTheme == appTheme;
+  }
+
+  @override
+  int get hashCode => Object.hash(locale, appTheme);
+
+  @override
+  String toString() =>
+      'SettingsState.idle(locale: $locale, appTheme: $appTheme)';
+}
+
+final class _ProcessingSettingsState extends SettingsState {
+  const _ProcessingSettingsState({super.locale, super.appTheme});
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is _ProcessingSettingsState &&
+        other.locale == locale &&
+        other.appTheme == appTheme;
+  }
+
+  @override
+  int get hashCode => Object.hash(locale, appTheme);
+
+  @override
+  String toString() =>
+      'SettingsState.processing(locale: $locale, appTheme: $appTheme)';
+}
+
+final class _ErrorSettingsState extends SettingsState {
+  const _ErrorSettingsState({
+    required this.cause,
+    super.locale,
+    super.appTheme,
+  });
+
+  /// The cause of the error.
+  final Object cause;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is _ErrorSettingsState &&
+        other.cause == cause &&
+        other.locale == locale &&
+        other.appTheme == appTheme;
+  }
+
+  @override
+  int get hashCode => Object.hash(cause, locale, appTheme);
+
+  @override
+  String toString() => 'SettingsState.error(cause: $cause, '
+      'locale: $locale, appTheme: $appTheme)';
+}
+
+/// Events for the [SettingsBloc].
+sealed class SettingsEvent {
+  const SettingsEvent();
+
+  /// Event to update theme.
+  const factory SettingsEvent.updateTheme({required AppTheme appTheme}) =
+      _UpdateThemeSettingsEvent;
+
+  /// Event to update the locale.
+  const factory SettingsEvent.updateLocale({required Locale locale}) =
+      _UpdateLocaleSettingsEvent;
+}
+
+final class _UpdateThemeSettingsEvent extends SettingsEvent {
+  const _UpdateThemeSettingsEvent({required this.appTheme});
+
+  /// The theme to update.
+  final AppTheme appTheme;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is _UpdateThemeSettingsEvent && other.appTheme == appTheme;
+  }
+
+  @override
+  int get hashCode => appTheme.hashCode;
+
+  @override
+  String toString() => 'SettingsEvent.updateTheme(appTheme: $appTheme)';
+}
+
+final class _UpdateLocaleSettingsEvent extends SettingsEvent {
+  const _UpdateLocaleSettingsEvent({required this.locale});
+
+  /// The locale to update.
+  final Locale locale;
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+
+    return other is _UpdateLocaleSettingsEvent && other.locale == locale;
+  }
+
+  @override
+  int get hashCode => locale.hashCode;
+
+  @override
+  String toString() => 'SettingsEvent.updateLocale(locale: $locale)';
 }
