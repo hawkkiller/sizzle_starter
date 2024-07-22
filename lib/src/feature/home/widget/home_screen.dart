331875da96181af6110e887ec1552f5efa863de0
Michael Lazebny <62852417+hawkkiller@users.noreply.github.com>
2024-07-22 21:52:31 +0200

    refactor: add WindowSize.fromWidth (#351)
    * refactor: add WindowSize.fromWidth
    
    * style: formatted code

--- lib/src/feature/home/widget/home_screen.dart
+++ lib/src/feature/home/widget/home_screen.dart
@@ -23,58 +23,55 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final windowWidth = MediaQuery.sizeOf(context).width;
+    final windowSize = WindowSize.fromWidth(windowWidth);
+
     return Scaffold(
       appBar: AppBar(title: const Text('Home Screen')),
       backgroundColor: Theme.of(context).colorScheme.surface,
-      body: LayoutBuilder(
-        builder: (context, constraints) {
-          final windowSize = constraints.materialBreakpoint;
-          return CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: Column(
-                  children: [
-                    const Text('Text scale'),
-                    Slider(
-                      divisions: 8,
-                      min: 0.5,
-                      max: 2,
-                      value: SettingsScope.textScaleOf(context).textScale,
-                      onChanged: (value) {
-                        SettingsScope.textScaleOf(context).setTextScale(value);
-                      },
-                    ),
-                  ],
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const Text('Text scale'),
+                Slider(
+                  divisions: 8,
+                  min: 0.5,
+                  max: 2,
+                  value: SettingsScope.textScaleOf(context).textScale,
+                  onChanged: (value) {
+                    SettingsScope.textScaleOf(context).setTextScale(value);
+                  },
                 ),
+              ],
+            ),
+          ),
+          SliverPadding(
+            padding: HorizontalSpacing.centered(windowWidth),
+            sliver: SliverGrid.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                crossAxisCount: switch (windowSize) {
+                  WindowSize.compact => 2,
+                  <= WindowSize.expanded => 3,
+                  _ => 4,
+                },
               ),
-              SliverPadding(
-                padding: HorizontalSpacing.centered(windowWidth),
-                sliver: SliverGrid.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisSpacing: 16,
-                    mainAxisSpacing: 16,
-                    crossAxisCount: switch (windowSize) {
-                      WindowSize.compact => 2,
-                      <= WindowSize.expanded => 3,
-                      _ => 4,
-                    },
-                  ),
-                  itemBuilder: (context, index) => ColoredBox(
-                    color: Theme.of(context).colorScheme.primary,
-                    child: Center(
-                      child: Text(
-                        'Item $index',
-                        style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                              color: Theme.of(context).colorScheme.onPrimary,
-                            ),
-                      ),
-                    ),
+              itemBuilder: (context, index) => ColoredBox(
+                color: Theme.of(context).colorScheme.primary,
+                child: Center(
+                  child: Text(
+                    'Item $index',
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                          color: Theme.of(context).colorScheme.onPrimary,
+                        ),
                   ),
                 ),
               ),
-            ],
-          );
-        },
+            ),
+          ),
+        ],
       ),
     );
   }
