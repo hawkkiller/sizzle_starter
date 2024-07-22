ca2f11db1c11bdf3e322718a83ff1274412e5973
Michael Lazebny <michael@lazebny.io>
2024-07-22 21:47:03 +0200

    style: formatted code

--- lib/src/feature/home/widget/home_screen.dart
+++ lib/src/feature/home/widget/home_screen.dart
@@ -29,50 +29,50 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(title: const Text('Home Screen')),
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: Column(
-                  children: [
-                    const Text('Text scale'),
-                    Slider(
-                      divisions: 8,
-                      min: 0.5,
-                      max: 2,
-                      value: SettingsScope.textScaleOf(context).textScale,
-                      onChanged: (value) {
-                        SettingsScope.textScaleOf(context).setTextScale(value);
-                      },
-                    ),
-                  ],
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const Text('Text scale'),
+                Slider(
+                  divisions: 8,
+                  min: 0.5,
+                  max: 2,
+                  value: SettingsScope.textScaleOf(context).textScale,
+                  onChanged: (value) {
+                    SettingsScope.textScaleOf(context).setTextScale(value);
+                  },
                 ),
-              ),
-              SliverPadding(
-                padding: HorizontalSpacing.centered(windowWidth),
-                sliver: SliverGrid.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisSpacing: 16,
-                    mainAxisSpacing: 16,
-                    crossAxisCount: switch (windowSize) {
-                      WindowSize.compact => 2,
-                      <= WindowSize.expanded => 3,
-                      _ => 4,
-                    },
-                  ),
-                  itemBuilder: (context, index) => ColoredBox(
-                    color: Theme.of(context).colorScheme.primary,
-                    child: Center(
-                      child: Text(
-                        'Item $index',
-                        style: Theme.of(context).textTheme.headlineSmall?.copyWith(
-                              color: Theme.of(context).colorScheme.onPrimary,
-                            ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
+          SliverPadding(
+            padding: HorizontalSpacing.centered(windowWidth),
+            sliver: SliverGrid.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisSpacing: 16,
+                mainAxisSpacing: 16,
+                crossAxisCount: switch (windowSize) {
+                  WindowSize.compact => 2,
+                  <= WindowSize.expanded => 3,
+                  _ => 4,
+                },
+              ),
+              itemBuilder: (context, index) => ColoredBox(
+                color: Theme.of(context).colorScheme.primary,
+                child: Center(
+                  child: Text(
+                    'Item $index',
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                          color: Theme.of(context).colorScheme.onPrimary,
+                        ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
