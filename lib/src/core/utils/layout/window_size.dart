5ff987703c0a9b782f78df9fcbc334879b321643
Michael Lazebny <62852417+hawkkiller@users.noreply.github.com>
2024-08-19 17:35:17 +0200

    feat: implemented WindowSize scope (#362)

--- lib/src/core/utils/layout/window_size.dart
+++ lib/src/core/utils/layout/window_size.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
 /// A breakpoint that is used to determine the layout of the application.
 ///
 /// It follows the Material Design guidelines for breakpoints.
@@ -55,15 +58,15 @@ enum WindowSize {
 
   /// Returns whether the given width isless than
   /// the minimum width of the breakpoint.
-  bool operator <(WindowSize other) => max < other.min;
+  bool operator <(WindowSize other) => min < other.min;
 
   /// Returns whether the given width is greater than
   /// the maximum width of the breakpoint.
-  bool operator >(WindowSize other) => min > other.max;
+  bool operator >(WindowSize other) => min > other.min;
 
   /// Returns whether the given width is less than
   /// or equal to the maximum width of the breakpoint.
-  bool operator <=(WindowSize other) => max <= other.max;
+  bool operator <=(WindowSize other) => min <= other.min;
 
   /// Returns whether the given width is greater than
   /// or equal to the minimum width of the breakpoint.
@@ -72,17 +75,108 @@ enum WindowSize {
   /// If the breakpoint is compact.
   bool get isCompact => this == WindowSize.compact;
 
+  /// If the breakpoint is compact or larger.
+  bool get isCompactUp => this >= WindowSize.compact;
+
   /// If the breakpoint is medium.
   bool get isMedium => this == WindowSize.medium;
+
+  /// If the breakpoint is medium or larger.
+  bool get isMediumUp => this >= WindowSize.medium;
 
   /// If the breakpoint is expanded.
   bool get isExpanded => this == WindowSize.expanded;
 
+  /// If the breakpoint is expanded or larger.
+  bool get isExpandedUp => this >= WindowSize.expanded;
+
   /// If the breakpoint is large.
   bool get isLarge => this == WindowSize.large;
+
+  /// If the breakpoint is large or larger.
+  bool get isLargeUp => this >= WindowSize.large;
 
   /// If the breakpoint is extra-large.
   bool get isExtraLarge => this == WindowSize.extraLarge;
 
+  /// If the breakpoint is extra-large or larger.
+  bool get isExtraLargeUp => this >= WindowSize.extraLarge;
+
   const WindowSize._(this.min, this.max);
+}
+
+/// Scope that provides [WindowSize] to its descendants.
+class WindowSizeScope extends StatefulWidget {
+  /// Creates a [WindowSizeScope] that provides [WindowSize] to its descendants.
+  const WindowSizeScope({
+    required this.child,
+    super.key,
+  });
+
+  /// The widget below this widget in the tree.
+  final Widget child;
+
+  /// Returns the [WindowSize] of the nearest [WindowSizeScope] ancestor.
+  static WindowSize of(BuildContext context, {bool listen = true}) {
+    final inherited = listen
+        ? context.dependOnInheritedWidgetOfExactType<_InheritedWindowSize>()
+        : context.findAncestorWidgetOfExactType<_InheritedWindowSize>();
+
+    return inherited?.windowSize ??
+        (throw FlutterError('WindowSizeScope was not found in the widget tree'));
+  }
+
+  @override
+  State<WindowSizeScope> createState() => _WindowSizeScopeState();
+}
+
+class _WindowSizeScopeState extends State<WindowSizeScope> with WidgetsBindingObserver {
+  late WindowSize _windowSize;
+
+  Size _getScreenSize() {
+    final view = PlatformDispatcher.instance.views.first;
+    return view.physicalSize / view.devicePixelRatio;
+  }
+
+  @override
+  void initState() {
+    _windowSize = WindowSize.fromWidth(_getScreenSize().width);
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangeMetrics() {
+    final windowSize = WindowSize.fromWidth(_getScreenSize().width);
+
+    if (_windowSize != windowSize) {
+      setState(() => _windowSize = windowSize);
+    }
+    super.didChangeMetrics();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => _InheritedWindowSize(
+        windowSize: _windowSize,
+        child: widget.child,
+      );
+}
+
+class _InheritedWindowSize extends InheritedWidget {
+  const _InheritedWindowSize({
+    required this.windowSize,
+    required super.child,
+  });
+
+  /// The [WindowSize] provided by this scope.
+  final WindowSize windowSize;
+
+  @override
+  bool updateShouldNotify(_InheritedWindowSize oldWidget) => windowSize != oldWidget.windowSize;
 }
