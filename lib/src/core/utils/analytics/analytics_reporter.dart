cc914bd8c0edee03caabe277cd0422644417ad75
Michael Lazebny <michael@lazebny.io>
2024-10-05 14:59:57 +0200

    docs: added documentation

--- lib/src/core/utils/analytics/analytics_reporter.dart
+++ lib/src/core/utils/analytics/analytics_reporter.dart
@@ -3,35 +3,47 @@ import 'package:sizzle_starter/src/core/utils/analytics/firebase_analytics_repor
 
 /// {@template analytics_reporter}
 /// Interface for reporting analytics events.
-/// 
+///
 /// This interface should be implemented to report [AnalyticsEvent]s to the
 /// analytics service being used by the application.
-/// 
+///
 /// See implementations of this interface:
 /// - [FirebaseAnalyticsReporter]
 /// {@endtemplate}
 abstract interface class AnalyticsReporter {
   /// Logs the provided [event] to analytics.
-  /// 
+  ///
   /// This method should be implemented to report the event to the analytics
   /// service being used by the application.
-  /// 
+  ///
   /// The [event] should be logged to the analytics service as-is, including any
   /// parameters that are included with the event.
   Future<void> logEvent(AnalyticsEvent event);
 }
 
 /// {@template analytics_event}
-/// Event that can be logged to analytics by [AnalyticsReporter].
+/// Represents an event that can be logged to analytics by [AnalyticsReporter].
+///
+/// This class can be used to track user interactions, screen views, or other
+/// significant actions within the application.
+///
+/// It is recommended to create custom events by extending this class, although
+/// events can also be added directly using this class.
 /// {@endtemplate}
 base class AnalyticsEvent {
   /// {@macro analytics_event}
   const AnalyticsEvent(this.name, {this.parameters});
 
   /// The name of the event.
+  ///
+  /// It should be a unique identifier for the event that is understood by the
+  /// analytics service being used.
   final String name;
 
   /// The parameters of the event.
+  ///
+  /// Parameters are optional and can be used to provide additional context or
+  /// data with the event.
   final Set<AnalyticsProperty<Object>>? parameters;
 
   @override
@@ -60,11 +72,11 @@ base class AnalyticsEvent {
 
 /// {@template analytics_property}
 /// A property that can be added to an [AnalyticsEvent].
-/// 
+///
 /// Currently, there are two types of properties:
 /// - [StringAnalyticsProperty]
 /// - [NumberAnalyticsProperty]
-/// 
+///
 /// Other types are not supported by Firebase Analytics. If you are using a
 /// different tool for analytics, you can create a custom property type.
 /// {@endtemplate}
