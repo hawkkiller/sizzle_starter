2a4f4c08e108d1bfcd45e48a7cc437e32db4f443
Michael Lazebny <michael@lazebny.io>
2024-11-27 11:50:24 +0100

    Updated docs

--- lib/src/feature/initialization/model/dependencies_container.dart
+++ lib/src/feature/initialization/model/dependencies_container.dart
@@ -2,14 +2,10 @@ import 'package:package_info_plus/package_info_plus.dart';
 import 'package:sizzle_starter/src/core/constant/application_config.dart';
 import 'package:sizzle_starter/src/core/utils/error_tracking_manager/error_tracking_manager.dart';
 import 'package:sizzle_starter/src/core/utils/logger/logger.dart';
-import 'package:sizzle_starter/src/feature/initialization/logic/composition_root.dart';
 import 'package:sizzle_starter/src/feature/settings/bloc/app_settings_bloc.dart';
 
 /// {@template dependencies_container}
-/// Composed dependencies from the [CompositionRoot].
-///
-/// This class contains all the dependencies that are required for the application
-/// to work.
+/// Container used to reuse dependencies across the application.
 ///
 /// {@macro composition_process}
 /// {@endtemplate}
