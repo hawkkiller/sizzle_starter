43aaec4f7373fa25dfad6f576e98216097c94d07
Michael Lazebny <miskadl09@gmail.com>
2025-01-01 13:29:12 +0100

    Formatted

--- lib/src/feature/settings/data/app_settings_datasource.dart
+++ lib/src/feature/settings/data/app_settings_datasource.dart
@@ -3,9 +3,9 @@ import 'dart:ui';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sizzle_starter/src/core/utils/color_codec.dart';
 import 'package:sizzle_starter/src/core/utils/persisted_entry.dart';
-import 'package:sizzle_starter/src/feature/settings/model/app_theme.dart';
 import 'package:sizzle_starter/src/feature/settings/data/theme_mode_codec.dart';
 import 'package:sizzle_starter/src/feature/settings/model/app_settings.dart';
+import 'package:sizzle_starter/src/feature/settings/model/app_theme.dart';
 
 /// {@template app_settings_datasource}
 /// [AppSettingsDatasource] sets and gets app settings.
