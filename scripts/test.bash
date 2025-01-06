f77e60428c9103a4c92d172ea04172b506af5558
Michael Lazebny <62852417+hawkkiller@users.noreply.github.com>
2025-01-06 15:43:11 +0100

    Update scripts (#392)
    * Update scripts
    
    * Update script
    
    * update ci
    
    * bootstrap
    
    * Updated bootstrap
    
    * Fix

---
+++ scripts/test.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Enable error handling
+set -e
+
+# Find directories with a pubspec.yaml and a test/ folder
+find_test_dirs() {
+  find . -type f -name "pubspec.yaml" -exec dirname {} \; | while read -r dir; do
+    if [ -d "$dir/test" ]; then
+      echo "$dir"
+    fi
+  done
+}
+
+# Capture the output of find_test_dirs and pass it to flutter test
+test_dirs=$(find_test_dirs)
+if [ -n "$test_dirs" ]; then
+  flutter test $test_dirs --no-pub --coverage
+else
+  echo "No directories with pubspec.yaml and test/ folder found."
+fi
