6a338e2290191b632af43fbed390943939cdede9
Michael Lazebny <miskadl09@gmail.com>
2025-05-20 18:40:40 +0200

    Update CI workflow to use a dedicated formatting script
    - Replaced inline formatting command in CI with a call to a new `format.bash` script.
    - Added `format.bash` script to format Dart files in all packages with a `pubspec.yaml` file, ensuring consistent code style across the project.

---
+++ scripts/format.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Run dart format in all packages of the project that have a pubspec.yaml file
+find app packages -type f -name "pubspec.yaml" -exec dirname {} \; | while read -r dir; do
+  if [ -f "$dir/pubspec.yaml" ]; then
+    pushd $dir
+    printf "\nFormatting $dir\n"
+
+    dirs=(lib test)
+
+    targets=()
+    for d in "${dirs[@]}"; do
+      [[ -d $d ]] && targets+=("$d")
+    done
+
+    # Only run find if at least one target exists
+    if ((${#targets[@]})); then
+      find "${targets[@]}" \
+        -name '*.dart' ! -name '*.*.dart' \
+        -exec dart format --line-length 100 {} +
+    fi
+    popd
+  fi
+done
