21183db48c31a6eb2cf918527bde6e61deda9d87
Michael Lazebny <miskadl09@gmail.com>
2025-06-16 14:25:50 +0200

    Update visibility

--- feature/settings/settings_api/lib/src/injection.dart
+++ feature/settings/settings_api/lib/src/injection.dart
@@ -3,7 +3,7 @@ import 'package:settings_api/src/data/datasources/settings_local_datasource.dart
 import 'package:settings_api/src/data/repositories/settings_repository_impl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-SettingsRepository createSettingsRepository(SharedPreferencesAsync sharedPreferences) {
+SettingsRepository _createSettingsRepository(SharedPreferencesAsync sharedPreferences) {
   return SettingsRepositoryImpl(
     settingsLocalDatasource: SettingsLocalDatasourceSharedPreferences(
       sharedPreferences: sharedPreferences,
@@ -11,7 +11,7 @@ SettingsRepository createSettingsRepository(SharedPreferencesAsync sharedPrefere
   );
 }
 
-Future<SettingsBloc> createSettingsBloc(SettingsRepository settingsRepository) async {
+Future<SettingsBloc> _createSettingsBloc(SettingsRepository settingsRepository) async {
   final settings = await settingsRepository.read();
   final initialState = SettingsState.idle(settings: settings);
 
@@ -32,8 +32,8 @@ class SettingsContainer {
   final SettingsBloc settingsBloc;
 
   static Future<SettingsContainer> create(SharedPreferencesAsync sharedPreferences) async {
-    final settingsRepository = createSettingsRepository(sharedPreferences);
-    final settingsBloc = await createSettingsBloc(settingsRepository);
+    final settingsRepository = _createSettingsRepository(sharedPreferences);
+    final settingsBloc = await _createSettingsBloc(settingsRepository);
 
     return SettingsContainer(
       settingsRepository: settingsRepository,
