cacffd394639608be6cdf3628351f2724a5bcc95
Michael Lazebny <miskadl09@gmail.com>
2025-09-28 19:46:01 +0200

    refactor: make material context stateless

--- app/lib/src/widget/material_context.dart
+++ app/lib/src/widget/material_context.dart
@@ -4,18 +4,13 @@ import 'package:settings_api/settings_api.dart';
 import 'package:sizzle_starter/src/widget/media_query_override.dart';
 
 /// Entry point for the application that uses [MaterialApp].
-class MaterialContext extends StatefulWidget {
+class MaterialContext extends StatelessWidget {
   const MaterialContext({super.key});
 
   /// This global key is needed for Flutter to work properly
   /// when Widgets Inspector is enabled.
   static final _globalKey = GlobalKey(debugLabel: 'MaterialContext');
 
-  @override
-  State<MaterialContext> createState() => _MaterialContextState();
-}
-
-class _MaterialContextState extends State<MaterialContext> {
   @override
   Widget build(BuildContext context) {
     return SettingsBuilder(
@@ -40,7 +35,7 @@ class _MaterialContextState extends State<MaterialContext> {
           home: const HomeScreen(),
           builder: (context, child) {
             return KeyedSubtree(
-              key: MaterialContext._globalKey,
+              key: _globalKey,
               child: MediaQueryRootOverride(child: child!),
             );
           },
