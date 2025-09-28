dc589690a5802a0101394bf00d8f95bb49c450c9
Michael Lazebny <miskadl09@gmail.com>
2025-09-28 13:04:21 +0200

    refactor: update settings repository

--- feature/settings/settings_api/lib/settings_api.dart
+++ feature/settings/settings_api/lib/settings_api.dart
@@ -1,6 +1,4 @@
-export 'src/application/bloc/settings_bloc.dart';
 export 'src/domain/model/settings.dart';
 export 'src/domain/model/theme_configuration.dart';
 export 'src/domain/repositories/settings_repository.dart';
 export 'src/injection.dart';
-export 'src/presentation/widget/settings_scope.dart';
--- feature/settings/settings_api/lib/src/injection.dart
+++ feature/settings/settings_api/lib/src/injection.dart
@@ -3,41 +3,21 @@ import 'package:settings_api/src/data/datasources/settings_local_datasource.dart
 import 'package:settings_api/src/data/repositories/settings_repository_impl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-SettingsRepository _createSettingsRepository(SharedPreferencesAsync sharedPreferences) {
-  return SettingsRepositoryImpl(
-    settingsLocalDatasource: SettingsLocalDatasourceSharedPreferences(
-      sharedPreferences: sharedPreferences,
-    ),
-  );
-}
-
-Future<SettingsBloc> _createSettingsBloc(SettingsRepository settingsRepository) async {
-  final settings = await settingsRepository.read();
-  final initialState = SettingsState.idle(settings: settings);
-
-  return SettingsBloc(
-    initialState: initialState,
-    settingsRepository: settingsRepository,
+Future<SettingsRepository> _createSettingsRepository(SharedPreferencesAsync sharedPreferences) {
+  return SettingsRepositoryImpl.init(
+    SettingsLocalDatasourceSharedPreferences(sharedPreferences: sharedPreferences),
   );
 }
 
 /// Container with global settings state.
 class SettingsContainer {
-  const SettingsContainer({
-    required this.settingsRepository,
-    required this.settingsBloc,
-  });
+  const SettingsContainer({required this.settingsRepository});
 
   final SettingsRepository settingsRepository;
-  final SettingsBloc settingsBloc;
 
   static Future<SettingsContainer> create(SharedPreferencesAsync sharedPreferences) async {
-    final settingsRepository = _createSettingsRepository(sharedPreferences);
-    final settingsBloc = await _createSettingsBloc(settingsRepository);
+    final settingsRepository = await _createSettingsRepository(sharedPreferences);
 
-    return SettingsContainer(
-      settingsRepository: settingsRepository,
-      settingsBloc: settingsBloc,
-    );
+    return SettingsContainer(settingsRepository: settingsRepository);
   }
 }
