9bff8e1928171d607d6cbcd702c916027225633d
Michael Lazebny <62852417+hawkkiller@users.noreply.github.com>
2025-10-05 20:59:58 +0200

    feat(settings): use SharedPreferencesColumnJson (#462)
    Use SharedPreferencesColumnJson to save settings as JSON.

--- feature/settings/lib/src/data/datasources/settings_local_datasource.dart
+++ feature/settings/lib/src/data/datasources/settings_local_datasource.dart
@@ -1,6 +1,6 @@
 import 'dart:async';
-import 'dart:convert';
 
+import 'package:common/common.dart';
 import 'package:settings/settings.dart';
 import 'package:settings/src/data/mappers/settings_codec.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -19,17 +19,23 @@ final class SettingsLocalDatasourceSharedPreferences implements SettingsLocalDat
   final SharedPreferencesAsync sharedPreferences;
   final SettingsCodec settingsCodec;
 
+  SharedPreferencesColumnJson get sharedPreferencesColumnJson => SharedPreferencesColumnJson(
+    sharedPreferences: sharedPreferences,
+    key: 'settings',
+  );
+
   @override
   Future<void> save(Settings settings) async {
     final settingsMap = settingsCodec.encode(settings);
-    await sharedPreferences.setString('settings', jsonEncode(settingsMap));
+
+    await sharedPreferencesColumnJson.set(settingsMap);
   }
 
   @override
   Future<Settings> read() async {
-    final settingsMap = await sharedPreferences.getString('settings');
+    final settingsMap = await sharedPreferencesColumnJson.read();
     if (settingsMap == null) return const Settings();
 
-    return settingsCodec.decode(jsonDecode(settingsMap) as Map<String, Object?>);
+    return settingsCodec.decode(settingsMap);
   }
 }
